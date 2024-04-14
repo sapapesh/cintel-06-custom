e12352675925dadc176099386a13c1814c3f7c5d
Sarah Howard <sapapesh21@gmail.com>
2024-04-13 21:12:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,7 @@ df = pd.DataFrame(diabetes.data,columns=diabetes.feature_names)
 df['target'] = diabetes.target
 
 #Print the first 5 rows of some variables
-print(df[['age', 'sex', 'bmi','target']].head())
-
-#Correlation Analysis of dataset
-corr = df[['age', 'sex', 'bmi', 'target']].corr()
-print(corr)
+print(df[['age', 'sex', 'bmi', 'bp', 's2', 's3', 's4', 's5', 's6', 'target']].head())
 
 #Create A1C readings
 UPDATE_INTERVAL_SECS: int = 60
@@ -47,20 +43,136 @@ def reactive_calc_combined():
 
 ui.page_opts(title="Diabetes Awareness Statistics", fillable=True)
 
+with ui.sidebar(open="open", style="font-family: 'Comic Sans MS'"):
+    ui.h2("Diabetes Contributing Factors", class_="text-center")
+    icon_svg("bomb")
+    ui.p(
+        "A demonstration of how various factors impact diabetes.",
+        class_="text-center",
+    )
+
+    ui.hr()
+
+    ui.input_checkbox_group(  
+    "checkbox_group",  
+        "Checkbox group",  
+        {  
+        "age":"Age",  
+        "sex": "Sex",  
+        "bmi": "Body Mass Index",
+        "s2": "LDL",
+        "s3": "HDL",
+        "s4": "Total Cholesterol",
+        "s6": "Blood Glucose Level"},)
+
+    ui.h6("Links:")
+    ui.a(
+        "GitHub Source",
+        href="https://https://https://github.com/sapapesh/cintel-06-custom",
+        target="_blank",
+    )
 
 with ui.layout_columns():
+    with ui.value_box(
+        showcase=icon_svg("arrow-right"),
+        style="background-color: gray; font-family: 'Comic Sans MS'",
+    ):
+        "A1C"
+        
+        @render.text
+        def display_A1C():
+            """Get the latest reading and return a temperature string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['A1C']}"
+
+        "Time Stamp"
+        @render.text
+        def display_time():
+            """Get the latest reading and return a timestamp string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['timestamp']}"
+
+with ui.card(full_screen=True):
+    ui.card_header("Current Readings", style="background-color: yellow; color: black",)
+
+    @render.data_frame
+    def display_df():
+        """Get the latest reading and return a dataframe with current readings"""
+        deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+        pd.set_option('display.width', None)        # Use maximum width
+        return render.DataGrid( df,width="100%")
+
+ui.hr()
+
+with ui.layout_columns(col_widths=(4, 8)):
+    with ui.card(full_screen=True):
+        "Diabetes Stats"
+
+    @render.data_frame
+    def diabetes_datatable():
+        return render.DataTable(filtered_data())
+
+with ui.card():
+    ui.card_header("Chart with Current Trend", style="background-color: lightgray; width: 100%,")
 
     @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y="tip")
+    def display_plot():
+        deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+        if not df.empty:
+           df["target"] = pd.to_datetime(df["target"]) 
+        
+        fig = px.scatter(df,
+            x="sex",
+            y="smoker",
+            title="Factors of sex and smoking",
+            labels={"sex": "Sex", "smoker": "Smoker"},
+            color_discrete_sequence=["blue"] )
 
-    @render_plotly
+        sequence = range(len(df))
+        x_vals = list(sequence)
+        y_vals = df["target"]
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x_vals, y_vals)
+        df['best_fit_line'] = [slope * x + intercept for x in x_vals]
+
+        fig.add_scatter(x=df["target"], y=df['best_fit_line'], mode='lines', name='Regression Line')
+        fig.update_layout(xaxis_title="Sex",yaxis_title="Smoker", yaxis_range=[])
+        fig.update_layout(font_color="black")
+        return fig
+
+@reactive.calc
+def filtered_data():
+    return diabetes.data[diabetes.data["sex"].isin(input_checkbox_group())]
     
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
+with ui.layout_columns(col_widths=(4, 8)):
+    with ui.card(full_screen=True): "Plotly Scatterplot"
+
+    @render_plotly
+    def plotly_scatterplot():
+        return px.scatter(filtered_data(),
+            x="sex",
+            y="bmi",
+            color="blood glucose",
+            title="Diabetes Factors",
+            labels={
+                "sex": "Sex",
+                "bmi": "Body Mass Index",
+            },
+            size_max=8, 
+        )
+
+@render_plotly
+def plot1():
+        return px.histogram(px.data.tips(), y="sex")
+
+@render_plotly
+    
+def plot2():
+        return px.histogram(px.data.tips(), y="smoker")
 
 #define a reactive calc to fake new data points and/or filter a data frame
 #define the Shiny Express UI
 #The overall page options
 #A sidebar
 #The main section with ui cards, value boxes, and space for grids and charts
+
